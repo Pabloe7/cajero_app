31aa97174033f0ae7b580aa7b9d655ce93cdf75f
Pabloe7 <pabloe.castiblancos@gmail.com>
2020-12-10 11:15:51 -0500

    Subiendo server

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'))
+res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
